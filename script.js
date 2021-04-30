57093fccca2d8ffe75c48b1cb9e27e18aca73e7a
Keseana Robinson <64967367+kesh4207@users.noreply.github.com>
2021-04-30 14:00:04 +0100

    Adding Tests to Currency Convertor

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-
-
-function poundsToDollars(amount){
-  //Coverting from pounds to dollars
-    return amount*1.39;
+function poundsToDollars(amount) {
+	//Coverting from pounds to dollars
+	return amount * 1.39;
 }
 
-
-
-function dollarsToPounds(amount){
-  //Converting from Dollars to Pounds
-  return amount*0.72;
+function dollarsToPounds(amount) {
+	//Converting from Dollars to Pounds
+	return amount * 0.72;
 }
-module.exports = (poundsToDollars, dollarsToPounds);
-
-
-
-
-
-
-
-
-
-
+module.exports = { poundsToDollars, dollarsToPounds };

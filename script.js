b6db16965d86c08ebb4aa72d3a9d29ace3dcbf95
Augustine129 <>
2021-04-30 10:26:23 +0000

    Add new functions for conversation

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function testCurrencyConverter(amount){
+  //Coverting from pounds to dollars
+    return amount*1.39;
+}
+
+
+
+function dollarsToPounds(amount){
+  //Converting from Dollars to Pounds
+  return amount*0.72;
+}
+module.exports = (testCurrencyConverter, dollarsToPounds);
+
+
+
+
+
+
+
+
+
+
